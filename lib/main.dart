f4b8d7b1e3651dfc1369b5afea88578d3ad8345a
AdityaBkIIITA <biswakarma1304@gmail.com>
2021-07-04 21:41:32 +0530

    Flutter commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playsound(int key) {
+    final player = AudioCache();
+    player.play('note$key.wav');
+  }
+
+  Expanded demo({Color color, int key}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playsound(key);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                demo(color: Colors.purple, key: 1),
+                demo(color: Colors.teal, key: 2),
+                demo(color: Colors.blue, key: 3),
+                demo(color: Colors.green, key: 4),
+                demo(color: Colors.yellow, key: 5),
+                demo(color: Colors.orange, key: 6),
+                demo(color: Colors.red, key: 7),
+              ],
+            ),
+          ),
         ),
       ),
     );
